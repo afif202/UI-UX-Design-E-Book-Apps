637c29776879587dfcbac2cf35e346cf60e51442
Afif <68719526+afif202@users.noreply.github.com>
2020-12-13 13:04:24 +0700

    Add files via upload

---
+++ Table_View/Table_View/PropinsiTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  PropinsiTableViewCell.swift
+//  Table_View
+//
+//  Created by macbook on 12/11/20.
+//
+
+import UIKit
+
+class PropinsiTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var propinsiView: UIView!
+    
+    @IBOutlet weak var propinsiimg: UIImageView!
+    
+    @IBOutlet weak var propinsiLbl: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
